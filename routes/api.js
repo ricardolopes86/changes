ae3df9d3556cc6cf782c12022fbd99e451fc584f
Ricardo Silva <ricardolopes18@yahoo.com.br>
2017-03-29 22:58:37 +0200

    Add change form created.

--- routes/api.js
+++ routes/api.js
@@ -5,4 +5,24 @@ router.get('/', function(req, res, next) {
     res.json();
 });
 
+router.get('/allevents', function(req, res, next) {
+    var eventos = [
+        {
+            title  : 'CM1234',
+            start  : '2017-03-01'
+        },
+        {
+            title  : 'CM4321',
+            start  : '2017-03-05',
+            end    : '2017-03-07'
+        },
+        {
+            title  : 'CM1423',
+            start  : '2017-03-09T12:30:00',
+            allDay : false // will make the time show
+        }
+    ];
+    res.json(eventos);
+});
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ router.get('/', function(req, res, next) {
     res.render('index');
 });
 
+router.get('/calendar', function(req, res, next) {
+    res.render('calendar');
+});
+
+router.get('/change/add', function(req, res, next) {
+    res.render('addchange');
+});
+
 module.exports = router;
