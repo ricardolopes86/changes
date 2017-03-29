8643d99c92712d82b4c04531612ffa54de71ebf4
Ricardo Silva <ricardolopes18@yahoo.com.br>
2017-03-29 21:57:10 +0200

    Initial Template created.

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var ejs = require('ejs');
+
+var index = require('./routes/index');
+
+//port
+var port = 3000;
+
+//init app
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(port, function (err) {
+    if(err){
+        console.log(err);
+    }
+});
+
+module.exports = app;
